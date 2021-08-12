27ddf3069852d3d4c915df60e3470e3faa7c40cb
BuildTools <ipagar@icloud.com>
2021-08-12 23:16:22 +0300

    add site metadata

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteMetadata: {
+        siteUrl: 'https://yourdomain.org',
+        title: 'Frontend Masters Intro',
+        description: 'Description',
+        image: 'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg'
+    }
+}
