9b3d3d548c0d41123477a599d3249b5bbc4dceaf
BuildTools <ipagar@icloud.com>
2021-08-13 00:10:02 +0300

    add metadata, add SEO component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,17 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import { Seo } from "../components/Seo.js"
 
 export default function AboutPage() {
     return (
-        <main>
-            <h1>About This Site</h1>
-            <Link to="/" >Back To Home</Link>
-        </main>
+        <>
+            <Seo
+                title="About This Site"
+                description="Description" />
+            <main>
+                <h1>About This Site</h1>
+                <Link to="/" >Back To Home</Link>
+            </main>
+        </>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
 import * as React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
+import { Seo } from '../components/seo.js'
 
 export default function IndexPage() {
     const data = useStaticQuery(graphql`
         query GetTitleSite {
-            site(siteMetadata: {}) {
+            site {
                 siteMetadata {
-                    title
+                    title      
                 }
-            }
+            } 
         }
     `)
 
@@ -16,6 +17,7 @@ export default function IndexPage() {
 
     return (
         <>
+            <Seo />
             <header>
                 <Link to="/">{meta.title}</Link>
             </header>
