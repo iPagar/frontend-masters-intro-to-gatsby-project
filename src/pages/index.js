d420bbff469934b0147a043719ff7250c3113f3c
BuildTools <ipagar@icloud.com>
2021-08-12 22:58:48 +0300

    add links between pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 export default function IndexPage() {
     return (
         <main>
             <h1>Hello!</h1>
+            <Link to="/about" >About This Site</Link>
         </main>
     )
 }
