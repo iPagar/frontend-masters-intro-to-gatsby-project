24900ac28f2ad947349a52bb45ca2b0c72336d18
BuildTools <ipagar@icloud.com>
2021-08-13 12:27:00 +0300

    add MDX support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,33 @@
 module.exports = {
-    siteMetadata: {
-        siteUrl: 'https://yourdomain.org',
-        title: 'Frontend Masters Intro',
-        description: 'Description',
-        image: 'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg'
+  siteMetadata: {
+    siteUrl: 'https://yourdomain.org',
+    title: 'Frontend Masters Intro',
+    description: 'Description',
+    image:
+      'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/src/posts`,
+      },
     },
-    plugins: ['gatsby-plugin-react-helmet']
-}
+    {
+      resolve: 'gatsby-plugin-page-creator',
+      options: {
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          posts: require.resolve('./src/components/posts-layout.js'),
+        },
+      },
+    },
+  ],
+};
