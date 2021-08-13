24fd1973dc7a7226b0511a492b8c15df5d56393c
BuildTools <ipagar@icloud.com>
2021-08-13 12:42:21 +0300

    add a post listing on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,38 @@
-import * as React from 'react'
-import { Link } from 'gatsby'
-import Layout from '../components/layout.js'
+import * as React from 'react';
+import { Link, useStaticQuery, graphql } from 'gatsby';
+import Layout from '../components/layout.js';
 
 export default function IndexPage() {
+  const data = useStaticQuery(graphql`
+    query GetPosts {
+      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+        nodes {
+          id
+          slug
+          frontmatter {
+            date(fromNow: true)
+            title
+            description
+          }
+        }
+      }
+    }
+  `);
 
-    return (
-        <Layout>
-            <h1>Hello!</h1>
-            <Link to="/about" >About This Site</Link>
-        </Layout>
-    )
+  const posts = data.allMdx.nodes;
+
+  return (
+    <Layout>
+      <h1>Hello!</h1>
+      <Link to="/about">About This Site</Link>
+
+      <h2>Check out my recent blog posts</h2>
+      {posts.map((post) => (
+        <li key={post.id}>
+          <Link to={post.slug}>{post.frontmatter.title}</Link>{' '}
+          <small>posted {post.frontmatter.date}</small>
+        </li>
+      ))}
+    </Layout>
+  );
 }
